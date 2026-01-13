f74e9e406deb16949e077a9caa20f27459ac8bab
= <=>
2026-01-13 09:06:35 -0300

    atualizando o card de anoversário

--- Aniversario/app/src/main/java/com/example/aniversario/MainActivity.kt
+++ Aniversario/app/src/main/java/com/example/aniversario/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    GreetingImage(
                         mensagem = "Feliz Aniversário Noeme",
                         ass = "por - Cristhiano"
                     )
@@ -47,14 +51,14 @@ fun GreetingText(mensagem: String, ass: String, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         modifier = modifier
             .padding(8.dp)
-            .background(color = MaterialTheme.colorScheme.tertiaryContainer)
     ) {
         Text(
             text = mensagem,
             fontSize = 70.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.headlineLarge
+            style = MaterialTheme.typography.headlineLarge,
+
         )
         Text(
             text = ass,
@@ -67,13 +71,32 @@ fun GreetingText(mensagem: String, ass: String, modifier: Modifier = Modifier) {
         )
     }
 }
-
+@Composable
+fun GreetingImage(mensagem: String, ass: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box (modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        GreetingText(
+            mensagem = mensagem,
+            ass = ass,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun AniversarioPreview() {
     AniversarioTheme {
-        GreetingText(
-            mensagem = "Feliz Aniversário Noeme",
+        GreetingImage(
+            mensagem = stringResource(R.string.feliz_aniversario_text),
             ass = "por - Cristhiano"
         )
     }
